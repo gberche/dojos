2b2bdcb45034b024935ff2d9fc66f75b5ad9da72
skwg9735 <guillaume.berche@example.com>
2013-01-21 22:29:46 +0100

    Now instanciates FizzBuzz

--- fizzbuzz-babysteps/src/test/java/kata/FizzBuzzTest.java
+++ fizzbuzz-babysteps/src/test/java/kata/FizzBuzzTest.java
@@ -1,11 +1,20 @@
 package kata;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Edited in at-home2
  */
 public class FizzBuzzTest {
+
+    private FizzBuff fizzBuff;
+
+    @Before
+    public void setUp() {
+        fizzBuff = new FizzBuff();
+    }
+
 
     @Test
     public void fizzBuzzShouldInstanciate() {
