958f6bc8942741e60ff1246abce4c070a22f8ab7
skwg9735 <guillaume.berche@example.com>
2013-01-21 22:41:55 +0100

    expected lines

--- fizzbuzz-babysteps/src/test/java/kata/FizzBuzzTest.java
+++ fizzbuzz-babysteps/src/test/java/kata/FizzBuzzTest.java
@@ -24,5 +24,31 @@ public class FizzBuzzTest {
         assertThat(nextLine).isNotNull();
     }
 
+    @Test
+    public void fizzBuzzShouldReturnExpectedLines() {
+        String [] lines = {"1\n",
+                "2\n",
+                "Fizz\n",
+                "4\n",
+                "Buzz\n",
+                "Fizz\n",
+                "7\n",
+                "8\n",
+                "Fizz\n",
+                "Buzz\n",
+                "11\n",
+                "Fizz\n",
+                "13\n",
+                "14\n",
+                "FizzBuzz\n",
+                "16\n",
+                "17\n",
+                "Fizz\n",
+                "19\n",
+                "Buzz"};
+        String nextLine = fizzBuff.getNextLine();
+        assertThat(nextLine).isNotNull();
+    }
+
 
 }
