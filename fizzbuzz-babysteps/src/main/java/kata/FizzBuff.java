7a4712f0c9fe4b579a916c2ce17f12f4f36d46d4
skwg9735 <guillaume.berche@example.com>
2013-01-21 23:25:58 +0100

    Refactored

--- fizzbuzz-babysteps/src/main/java/kata/FizzBuff.java
+++ fizzbuzz-babysteps/src/main/java/kata/FizzBuff.java
@@ -7,12 +7,16 @@ public class FizzBuff {
     int currentIndex = 1;
     public String getNextLine() {
         String currentLine;
-        if (currentIndex %3 == 0) {
+        boolean multipleOfThree = currentIndex % 3 == 0;
+        boolean multipleOf5 = currentIndex % 5 == 0;
+        if (multipleOfThree) {
             currentLine = "Fizz";
-        } else if (currentIndex %5 == 0) {
-            currentLine = "Buzz";
         } else {
-            currentLine = Integer.toString(currentIndex) ;
+            if (multipleOf5) {
+                currentLine = "Buzz";
+            } else {
+                currentLine = Integer.toString(currentIndex) ;
+            }
         }
         currentIndex++;
         return currentLine+ "\n";
