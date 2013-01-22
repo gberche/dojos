1b988604d329a5ad9125fb575b4e952ee005e0e9
skwg9735 <guillaume.berche@example.com>
2013-01-22 12:51:59 +0100

    impl supporting 3 and 5

--- fizzbuzz-babysteps/src/main/java/kata/FizzBuzz.java
+++ fizzbuzz-babysteps/src/main/java/kata/FizzBuzz.java
@@ -6,16 +6,15 @@ package kata;
 public class FizzBuzz {
     public String buildValue(int i) {
         boolean multiple_of_three = i % 3 == 0;
-        if (multiple_of_three) {
+        boolean multiple_of_five = i % 5 == 0;
+        if (multiple_of_three && multiple_of_five) {
+            return "FizzBuzz";
+        } else         if (multiple_of_three) {
             return "Fizz";
-
-        } else {
-            boolean multiple_of_five = i % 5 == 0;
-            if (multiple_of_five) {
+        } else if (multiple_of_five) {
                 return "Buzz";
-            } else {
+        } else {
                  return Integer.toString(i);
             }
-        }
     }
 }
