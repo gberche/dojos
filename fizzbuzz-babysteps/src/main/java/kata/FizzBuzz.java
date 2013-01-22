14dc4d08bb6b4e489e061e771c5a8c0708b06589
skwg9735 <guillaume.berche@example.com>
2013-01-22 12:50:00 +0100

    gestion jusqu'à 14

--- fizzbuzz-babysteps/src/main/java/kata/FizzBuzz.java
+++ fizzbuzz-babysteps/src/main/java/kata/FizzBuzz.java
@@ -7,6 +7,9 @@ public class FizzBuzz {
     public String buildValue(int i) {
         if (i %3 ==0) {
             return "Fizz";
+
+        } else if (i %5 ==0) {
+            return "Buzz";
         } else {
              return Integer.toString(i);
         }
