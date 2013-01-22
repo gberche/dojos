b7ef895f31e92c2660bb503d37251818d4b18905
skwg9735 <guillaume.berche@example.com>
2013-01-22 12:47:02 +0100

    renvoit la valeur du chiffre

--- fizzbuzz-babysteps/src/main/java/kata/FizzBuzz.java
+++ fizzbuzz-babysteps/src/main/java/kata/FizzBuzz.java
@@ -5,11 +5,6 @@ package kata;
  */
 public class FizzBuzz {
     public String buildValue(int i) {
-              if (i == 1)  {
-
-                  return "1";
-              }          else {
-                  return "2";
-              }
+             return Integer.toString(i);
     }
 }
