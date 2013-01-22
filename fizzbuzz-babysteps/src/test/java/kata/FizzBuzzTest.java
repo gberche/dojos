250a0ae8bf87cf81d4a777ec088facaf5971f92b
skwg9735 <guillaume.berche@example.com>
2013-01-22 12:31:13 +0100

    list of expecteds

--- fizzbuzz-babysteps/src/test/java/kata/FizzBuzzTest.java
+++ fizzbuzz-babysteps/src/test/java/kata/FizzBuzzTest.java
@@ -9,6 +9,25 @@ public class FizzBuzzTest {
 
     @Test
     public void test() {
-
+        String [] expected = {"1\n" ,
+                "2\n" ,
+                "Fizz\n" ,
+                "4\n" ,
+                "Buzz\n" ,
+                "Fizz\n" ,
+                "7\n" ,
+                "8\n" ,
+                "Fizz\n" ,
+                "Buzz\n" ,
+                "11\n" ,
+                "Fizz\n" ,
+                "13\n" ,
+                "14\n" ,
+                "FizzBuzz\n" ,
+                "16\n" ,
+                "17\n" ,
+                "Fizz\n" ,
+                "19\n" ,
+                "Buzz\n"};
     }
 }
