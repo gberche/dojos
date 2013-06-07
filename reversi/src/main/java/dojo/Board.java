798a480973b3353b2b8499fb66805d11ae3f3279
Guillaume Berche <nobody@nobody.com>
2013-06-07 23:33:36 +0200

    Implementation using inside-out tdd

--- reversi/src/main/java/dojo/Board.java
+++ reversi/src/main/java/dojo/Board.java
@@ -1,6 +1,9 @@
 package dojo;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 /**
  *
@@ -12,27 +15,90 @@ public class Board {
     protected static final char EMPTY_CELL = '.';
     protected static final char LEGAL_MOVE = '0';
 
+    static final Coordinate N = new Coordinate(-1, 0);
+    static final Coordinate S = new Coordinate(1, 0);
+    static final Coordinate E = new Coordinate(0, -1);
+    static final Coordinate W = new Coordinate(0, 1);
+    static final Coordinate NE = new Coordinate(-1, -1);
+    static final Coordinate SE = new Coordinate(1, 1);
+    static final Coordinate SW = new Coordinate(1, -1);
+    static final Coordinate NW = new Coordinate(-1, -1);
+
+    private List<Coordinate> LEGAL_DIRECTIONS = asList(N, S, E, W, NE, SE, SW, NW);
+
     public void initBoard(String initialState) {
     	cells = initialState.toCharArray();
     }
     
 	public String getNextLegalMoves(char currentPlayer) {
-        return null;
+        Board boardWithLegalMoves = new Board();
+        boardWithLegalMoves.initBoard(this.toString());
+
+        List<Coordinate> positionList = getCurrentPlayerCells(currentPlayer);
+
+        for (Coordinate currentPlayerCell : positionList) {
+            List<Coordinate> cellPositions = getNextLegalMovesFromPosition(currentPlayerCell);
+
+            boardWithLegalMoves.markCellsAsLegalMove(cellPositions);
+        }
+        return boardWithLegalMoves.toString();
 	}
 
+
+    protected List<Coordinate> getNextLegalMovesFromPosition(Coordinate currentPlayerCell) {
+        char currentPlayer = getCellAt(currentPlayerCell);
+        List<Coordinate> legalMoves = new ArrayList<Coordinate>();
+
+        List<Coordinate> directions = getLegalDirections();
+        for (Coordinate direction : directions) {
+            int contiguousOppositePiecesWithinLine = 0;
+            Coordinate neighbor = currentPlayerCell;
+
+            boolean neighborIsOppositePlayer;
+
+            do  {
+                neighbor = getNeighbor(neighbor, direction);
+                neighborIsOppositePlayer = isOppositePlayer(getCellAt(neighbor), currentPlayer);
+                if (neighborIsOppositePlayer) {
+                    contiguousOppositePiecesWithinLine ++;
+                }
+            } while (neighbor != null && neighborIsOppositePlayer);
+
+            if (contiguousOppositePiecesWithinLine >0 && neighbor!= null && getCellAt(neighbor)==EMPTY_CELL) {
+                legalMoves.add(neighbor);
+            }
+        }
+        return legalMoves;
+    }
+
+
+    protected Coordinate getNeighbor(Coordinate origin, Coordinate direction) {
+        int x = origin.x + direction.x;
+        int y = origin.y + direction.y;
+        if (x<0 || x > 7 || y <0 || y > 7) {
+            return null;
+        }
+        return new Coordinate(x, y);
+    }
+
+    protected List<Coordinate> getCurrentPlayerCells(char player) {
+        List<Coordinate> positions = new ArrayList<Coordinate>();
+        for (int i = 0; i < cells.length; i++) {
+            if (cells[i] == player) {
+                positions.add(getCellPositionFromCellArrayIndex(i));
+            }
+        }
+        return positions;
+    }
 
     private Coordinate getCellPositionFromCellArrayIndex(int index) {
         return new Coordinate(index/9, index%9);
     }
 
-    protected char getCellAt(Coordinate cellPosition) {
-        return cells[9*cellPosition.x + cellPosition.y];
-    }
-
-
     private void markCellAsLegalMove(Coordinate cellPosition) {
         cells[9*cellPosition.x + cellPosition.y] = LEGAL_MOVE;
     }
+
 
     protected void markCellsAsLegalMove(List<Coordinate> cellPositions) {
         for (Coordinate coordinate : cellPositions) {
@@ -43,6 +109,24 @@ public class Board {
     @Override
     public String toString() {
     	return new String(cells);
+    }
+
+    protected List<Coordinate> getLegalDirections() {
+        return LEGAL_DIRECTIONS;
+    }
+
+    protected char getCellAt(Coordinate cellPosition) {
+        return cells[9*cellPosition.x + cellPosition.y];
+    }
+
+    protected boolean isOppositePlayer(char cell, char currentPlayer) {
+        if (cell == currentPlayer) {
+            return false;
+        }
+        if (cell != LEGAL_MOVE && cell!= EMPTY_CELL) {
+            return true;
+        }
+        return false;
     }
 
 }
--- reversi/src/test/java/dojo/DojoTest.java
+++ reversi/src/test/java/dojo/DojoTest.java
@@ -3,6 +3,8 @@ package dojo;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static java.util.Arrays.asList;
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -12,6 +14,15 @@ import static org.fest.assertions.Assertions.assertThat;
 public class DojoTest {
 
     private static final char BLACK = 'B';
+    Coordinate N = new Coordinate(-1, 0);
+    Coordinate S = new Coordinate(1, 0);
+    Coordinate W = new Coordinate(0, -1);
+    Coordinate E = new Coordinate(0, 1);
+    Coordinate NE = new Coordinate(-1, -1);
+    Coordinate SE = new Coordinate(1, 1);
+    Coordinate SW = new Coordinate(1, -1);
+    Coordinate NW = new Coordinate(-1, -1);
+
     private static final String EXPECTED_STATE = "........\n" +
             "........\n" +
             "....0...\n" +
@@ -43,6 +54,57 @@ public class DojoTest {
         assertThat(nextState).isEqualTo(EXPECTED_STATE);
     }
 
+    @Test
+    public void it_fetches_neighbor_cell_or_null_if_out_of_board() {
+        assertThat(board.getNeighbor(new Coordinate(0,0), new Coordinate(0, 1))).isEqualTo(new Coordinate(0, 1));
+        assertThat(board.getNeighbor(new Coordinate(0,0), new Coordinate(0, -1))).isNull();
+        assertThat(board.getNeighbor(new Coordinate(0,0), new Coordinate(-1, 0))).isNull();
+        assertThat(board.getNeighbor(new Coordinate(0,7), new Coordinate(0, 1))).isNull();
+        assertThat(board.getNeighbor(new Coordinate(7,7), new Coordinate(0, 1))).isNull();
+    }
+
+    @Test
+    public void it_computes_next_possible_moves_from_a_position() {
+        board.initBoard(
+                "........\n" +
+                "........\n" +
+                "........\n" +
+                "...BW...\n" +
+                "...WB...\n" +
+                "........\n" +
+                "........\n" +
+                "........");
+        Coordinate currentPlayerCell = new Coordinate(3, 3);
+        List<Coordinate> expected = asList(new Coordinate(5, 3), new Coordinate(3, 5));
+        assertThat(board.getNextLegalMovesFromPosition(currentPlayerCell)).isEqualTo(expected);
+    }
+
+
+
+    @Test
+    public void it_compares_cells_content_to_determine_opposite_players() {
+        assertThat(board.isOppositePlayer('B', 'B')).isFalse();
+        assertThat(board.isOppositePlayer('W', 'W')).isFalse();
+        assertThat(board.isOppositePlayer('B', 'W')).isTrue();
+        assertThat(board.isOppositePlayer('W', 'B')).isTrue();
+        assertThat(board.isOppositePlayer(Board.EMPTY_CELL, 'W')).isFalse();
+        assertThat(board.isOppositePlayer(Board.EMPTY_CELL, 'W')).isFalse();
+        assertThat(board.isOppositePlayer(Board.LEGAL_MOVE, 'W')).isFalse();
+        assertThat(board.isOppositePlayer(Board.LEGAL_MOVE, 'B')).isFalse();
+    }
+
+    @Test
+    public void it_defines_legal_directions() {
+        List<Coordinate> legalDirections = board.getLegalDirections();
+
+        assertThat(legalDirections).isEqualTo(asList(N, S, W, E, NE, SE, SW, NW));
+    }
+
+    @Test
+    public void it_lists_current_player_cells() {
+        board.initBoard(INITIAL_STATE);
+        org.junit.Assert.assertEquals(asList(new Coordinate(3, 3), new Coordinate(4, 4)), board.getCurrentPlayerCells('B'));
+    }
 
     @Test
     public void it_produces_outputs_in_same_format_as_input() {
