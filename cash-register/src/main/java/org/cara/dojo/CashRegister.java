8207af4384c59649f69ceab2805e0ec76ea92c09
Guillaume Berche <guillaume.berche@orange.com>
2015-02-24 12:55:51 +0100

    move all methods to CashRegister

--- cash-register/src/main/java/org/cara/dojo/CashRegister.java
+++ cash-register/src/main/java/org/cara/dojo/CashRegister.java
@@ -1,7 +1,33 @@
 package org.cara.dojo;
 
+import java.util.List;
+
 /**
  *
  */
 public class CashRegister {
+    Float account(List<Denomination> denoms) {
+      float totalAmount = 0.0f;
+      for (Denomination denomination : denoms) {
+        totalAmount += denomination.amount();
+      }
+
+      return totalAmount;
+    }
+
+    Float getBalance(Float totalProducts, Float payment) {
+      return payment - totalProducts;
+    }
+
+    void checkBalance(Float balance) throws InsuffisantMoneyException {
+      if(balance <0.0f) {
+        throw new InsuffisantMoneyException();
+      }
+    }
+
+    Float makePayment(Float totalProducts, Float payment) throws InsuffisantMoneyException {
+        Float balance = getBalance(totalProducts, payment);
+        checkBalance(balance);
+        return balance;
+    }
 }
